c78eeef912b1c33d70237ef7859e1280ce6ca312
QTuan <tuanlqhe173471@fpt.edu.vn>
2026-08-18 11:30:24 +0700

    push code

--- KS_25_CNTT1/Session23/routers/auth_router.py
+++ KS_25_CNTT1/Session23/routers/auth_router.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter , Depends , HTTPException ,status
+from fastapi import APIRouter , Depends , HTTPException ,status , Form , UploadFile , File
 from schemals.auth_schemal import FormLoginRequest , LoginResponse
 from sqlalchemy.orm import Session
 from database import connect_db
 from services  import auth_service
+from fastapi.staticfiles import StaticFiles
+import os , uuid , shutil
 
 auth_router = APIRouter(prefix="/auth",tags=["Auth"])
 
@@ -18,3 +20,39 @@ def handle_login (data_form : FormLoginRequest , db: Session = Depends(connect_d
         )
     else :
         return data
+
+    
+@auth_router.post("/login_v2")
+def handle_login_v2(
+    username : str = Form(...),
+    password : str = Form(...)
+):
+    # login 
+    return {"message" : "Đăng nhập thành công" , "username" : username , "password" : password}
+
+
+
+DIR_UPLOAD_FILE = "upload/images"
+os.makedirs(DIR_UPLOAD_FILE,exist_ok=True)
+
+# Xây dựng một api làm việc với file , uploadfile , và lưu ảnh vào một folder tĩnh của dự án
+@auth_router.post("/upload_file_image")
+def handle_upload_image (file_image : UploadFile = File(...)):
+    # Kiểm tra xem file có phải file ảnh không
+    # Lấy đường dẫn cuối cùng trong ảnh 
+    type_img = file_image.filename.split(".")[-1]
+    if type_img != "png":
+        raise HTTPException(status_code=400)
+
+    # đặt lại tên ảnh cho đúng quy chuẩn hệ thống
+    new_name_file = f"{uuid.uuid4().hex}.{type_img}"
+
+    # Tạo URL trực tiếp  của ảnh này 
+    url_image_create = os.path.join(DIR_UPLOAD_FILE,new_name_file)
+    # upload/images/8be41a7273a344b0af64975e702b9e74.png
+
+    # Lưu lại ảnh vào folder tĩnh mà mình đã tạo
+    with open(url_image_create,"wb") as buffer:
+        shutil.copyfileobj(file_image.file,buffer)
+
+    return file_image
